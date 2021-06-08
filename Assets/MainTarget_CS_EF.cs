1e4903c9223ef500e23d584e78f8242492c48095
umbrahawk <scottalecmiller1995@gmail.com>
2021-06-08 21:25:49 +1000

    Basic Target Trigger
    Made a basic target that will move on a 90 degree angle which can be triggered on and off

---
+++ Assets/MainTarget_CS_EF.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTarget_CS_EF : MonoBehaviour
+{
+    public bool isActive = true;
+    public float lifetime = 5.0f;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("ToggleTarget");
+    }
+
+    IEnumerator ToggleTarget()
+    {
+        isActive = false;
+
+        float t = lifetime;
+
+        target.transform.Rotate(0, 0, 90);
+
+        while (t > 0)
+        {
+            t -= Time.deltaTime;
+
+            yield return null;
+
+            print(lifetime);
+        }
+
+
+        ActivateTarget();
+    }
+
+    public void ActivateTarget()
+    {
+        print("Running the program");
+
+        isActive = true;
+
+        target.transform.Rotate(0, 0, -90);
+
+    }
+}
