bc72646fc8920fcec77892722ae35546536ca156
umbrahawk <scottalecmiller1995@gmail.com>
2021-06-10 14:09:34 +1000

    Projectile Added
    Base level of a projectile is added into the project.
    Currently suspended in space, it'll shoot forward with the AddForce mechanic

---
+++ Assets/Scripts/Projectile_CS_EF.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_CS_EF : MonoBehaviour
+{
+    Rigidbody rb;
+    public int forwardSpeed = 2000;
+    public int upwardSpeed = 200;
+    public float lifetime = 2.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(transform.forward * forwardSpeed);
+
+        StartCoroutine("Lifetime");
+    }
+
+    IEnumerator Lifetime()
+    {
+        yield return new WaitForSeconds(lifetime);
+
+        Destroy(gameObject);
+    }
+}
